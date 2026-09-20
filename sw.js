58917ed4587f0d0455a9e54ddca8985f32a17c0f
melianiyanis9 <melianiyanis9@gmail.com>
2026-09-20 09:08:43 +0100

    Keep clean 100% stable PS4-HEN and remove experimental GoldHEN demo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'yova-flash-v5';
+const CACHE_NAME = 'yova-flash-v6';
 const ASSETS = [
   './',
   './index.html',
@@ -15,7 +15,6 @@ const ASSETS = [
   './ps4_offsets.js',
   './rpc_worker.js',
   './payload2.bin',
-  './goldhen.bin',
   './patches/1302.bin',
   './patches/1350.bin',
   './patches/1352.bin'
